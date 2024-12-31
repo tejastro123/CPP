eceb92b21364e3252f2662352c0fcb789a411bc9
TEJAS <mellimpudi.tejas@gmail.com>
2024-12-31 15:00:24 +0530

    ad

---
+++ 27.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+//C++ & STL(standard template library)
+/*1.containers 2.iterators 3.algorithms 4.functors*/
+
+//VECTOR (dynamic & resize)
+int main()
+{
+    vector<int> vec;
+
+}
+
