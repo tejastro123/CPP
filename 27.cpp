93fa3ff1c27532dd2aea20a22525360f50afa1d5
TEJAS <mellimpudi.tejas@gmail.com>
2024-12-31 15:11:39 +0530

    add

--- 27.cpp
+++ 27.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 #include<vector>
+using namespace std;
 //C++ & STL(standard template library)
 /*1.containers 2.iterators 3.algorithms 4.functors*/
 
 //VECTOR (dynamic & resize)
 int main()
 {
-    vector<int> vec;
-
+    vector<int> vec; //size=0
+    vec.push_back(1);
+     
 }
-
+    
