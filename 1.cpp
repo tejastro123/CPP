0ecb93a00cab2f0f979a8ef5c55ce17a620f3366
tejas mellimpudi <mellimpudi.tejas@gmail.com>
2025-01-04 21:51:25 +0530

    add

--- 1.cpp
+++ 1.cpp
@@ -1,27 +1,27 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     cout << "tejas.mellimpudi" << endl;
-//     int age;
-//     cout << "Enter the age : " << age << endl;
-//     cin >> age;
-//     char grade ='A';
-//     int value = grade;
-//     float pi = 3.14f;
-//     bool issafe = true;
-//     double price = 120.99;
-//     int nprice = (int)price;
+#include <iostream>
+using namespace std; 
+int main() {
+    cout << "tejas.mellimpudi" << endl;
+    int age;
+    cout << "Enter the age : " << age << endl;
+    cin >> age;
+    char grade ='A';
+    int value = grade;
+    float pi = 3.14f;
+    bool issafe = true;
+    double price = 120.99;
+    int nprice = (int)price;
 
-//     cout << "Age: " << age << endl;
-//     cout << "Grade: " << grade << endl;
-//     cout << "Value of Grade: " << value << endl;
-//     cout << "Pi: " << pi << endl;
-//     cout << "Is Safe: " << (issafe? "Yes" : "No") << endl;
-//     cout << "Price: " << price << endl;
-//     cout << "New Price:" << nprice << endl;
+    cout << "Age: " << age << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Value of Grade: " << value << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "Is Safe: " << (issafe? "Yes" : "No") << endl;
+    cout << "Price: " << price << endl;
+    cout << "New Price:" << nprice << endl;
     
-//     return 0;
+    return 0;
 
-// }
+}
 
 
