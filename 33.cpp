39cc720c9ecb9ce6d41e7d78aa0b6cfa48f011ca
tejas mellimpudi <mellimpudi.tejas@gmail.com>
2025-01-08 19:32:19 +0530

    add

---
+++ 33.cpp
@@ -0,0 +1,22 @@
+// MATHS FOR DSA (EUCLIDS ALGORITHM, SIEVE OF ERATOSTHENES, MODULAR ARITHMETICS)
+/*TOPICS:-
+1.prime logic & sieve of eratosthenes
+2.GCD eith euclid's algorithm &LCM
+3.digits in a number
+4.reverse a num
+5.check palindrome
+6.armstrong logic
+7.moular arithmetics
+8.fast exponentiation*/
+
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main()
+{
+    
+}
