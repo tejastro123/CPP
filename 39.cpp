2d6a200a3245d1f7f51d64a1ca428b2a60acbb96
tejas mellimpudi <mellimpudi.tejas@gmail.com>
2025-01-21 03:58:23 +0530

    add

--- 39.cpp
+++ 39.cpp
@@ -3,11 +3,11 @@
 nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + 
 nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.*/
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<set>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// #include<set>
+// using namespace std;
  
 // 1.brute force method 
 // vector<vector<int>> threesum(vector<int>& nums){  // TC: O( n^3 * log(unique triplets)) 
@@ -59,38 +59,38 @@ using namespace std;
 // SC: O(n^2) for storing unique triplets in set
 
 // 3. pointer approach
-vector<vector<int>> threesum(vector<int>& nums){
-    int n = nums.size();
-    vector<vector<int>> ans;
-    sort(nums.begin(), nums.end());
+// vector<vector<int>> threesum(vector<int>& nums){
+//     int n = nums.size();
+//     vector<vector<int>> ans;
+//     sort(nums.begin(), nums.end());
     
-    for(int i=0; i<n; i++){
-        if(i>0 && nums[i] == nums[i-1]) continue;
-        int j = i+1, k = n-1;
-        while(j<k){
-            int sum = nums[i] + nums[j] + nums[k];
-            if(sum < 0){
-                j++;
-            } else if(sum > 0){
-                k--;
-            } else {
-              ans.push_back({nums[i], nums[j],nums[k]});
-              j++; k--;
-              while(j<k && nums[j] == nums[j-1]) j++;
-            }
-        }
-    }
-    return ans;
-}
+//     for(int i=0; i<n; i++){
+//         if(i>0 && nums[i] == nums[i-1]) continue;
+//         int j = i+1, k = n-1;
+//         while(j<k){
+//             int sum = nums[i] + nums[j] + nums[k];
+//             if(sum < 0){
+//                 j++;
+//             } else if(sum > 0){
+//                 k--;
+//             } else {
+//               ans.push_back({nums[i], nums[j],nums[k]});
+//               j++; k--;
+//               while(j<k && nums[j] == nums[j-1]) j++;
+//             }
+//         }
+//     }
+//     return ans;
+// }
 // TC: O( nlogn + n^2 )
 // SC: O( unique triplets)
 
-int main(){
-    vector<int> nums = {-1, 0, 1, 2, -1, -4};
-    vector<vector<int>> ans = threesum(nums);
+// int main(){
+//     vector<int> nums = {-1, 0, 1, 2, -1, -4};
+//     vector<vector<int>> ans = threesum(nums);
 
-    for(auto triplet : ans){
-        cout<<triplet[0]<<" "<<triplet[1]<<" "<<triplet[2]<<endl;
-    }
-    return 0;
-}
+//     for(auto triplet : ans){
+//         cout<<triplet[0]<<" "<<triplet[1]<<" "<<triplet[2]<<endl;
+//     }
+//     return 0;
+// }
