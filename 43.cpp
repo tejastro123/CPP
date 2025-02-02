280e1ea7f62d1c505c7ba24a46056c5b8fc45fce
tejas mellimpudi <mellimpudi.tejas@gmail.com>
2025-02-02 09:34:40 +0530

    add

--- 43.cpp
+++ 43.cpp
@@ -1,62 +1,63 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
 // fibonacci series
-vector<int> fibonacci(int n) {
-    vector<int> fib(n);
-    fib[0] = 0;
-    fib[1] = 1;
-    for(int i = 2; i < n; i++)
-        fib[i] = fib[i-1] + fib[i-2];
-    return fib;
-}
+// vector<int> fibonacci(int n) {
+//     vector<int> fib(n);
+//     fib[0] = 0;
+//     fib[1] = 1;
+//     for(int i = 2; i < n; i++)
+//         fib[i] = fib[i-1] + fib[i-2];
+//     return fib;
+// }
 // TC = O(2^n - 1)*k & SC = O(n)
 
 // binary search using recursion algorithm
-bool binarySearch(vector<int> arr, int target, int st ,int end){
-    if(st<=end){
-        int mid = st + (end - st) / 2;
-        if(arr[mid]==target) return mid;
-        else if(arr[mid]<target)
-            return binarySearch(arr, target, mid+1, end);
-        else
-            return binarySearch(arr, target, st, mid-1);
-    }
-    return false;
-}
+// bool binarySearch(vector<int> arr, int target, int st ,int end){
+//     if(st<=end){
+//         int mid = st + (end - st) / 2;
+//         if(arr[mid]==target) return mid;
+//         else if(arr[mid]<target)
+//             return binarySearch(arr, target, mid+1, end);
+//         else
+//             return binarySearch(arr, target, st, mid-1);
+//     }
+//     return false;
+// }
 // TC = O(logn) & SC = O(logn)
 
 // is array sorted using 
-bool isSorted(vector<int> arr,int n){
-    if(n==0 || n==1) return true;
+// bool isSorted(vector<int> arr,int n){
+//     if(n==0 || n==1) return true;
     
-    return arr[n-1] >= arr[n-2] && isSorted(arr,n-1);
-}// TC = O(n) & SC = O(n)
+//     return arr[n-1] >= arr[n-2] && isSorted(arr,n-1);
+// }// TC = O(n) & SC = O(n)
 
-int main()
-{
-    int n ;
-    cout << "Enter the number of elements in the array: ";
-    cin >> n;
-    cout << "Fibonacci series: ";
-    vector<int> fib = fibonacci(n);
-    for(int i = 0; i < n; i++)
-        cout << fib[i] << " ";
-    cout << endl;
+// int main()
+// {
+//     int n ;
+//     cout << "Enter the number of elements in the array: ";
+//     cin >> n;
+//     cout << "Fibonacci series: ";
+//     vector<int> fib = fibonacci(n);
+//     for(int i = 0; i < n; i++)
+//         cout << fib[i] << " ";
+//     cout << endl;
     
-    vector<int> arr = { 2,4,5,9,3,2,7,8};
-    cout << "Binary Search: ";
-    int target;
-    cout << "Enter the target element to search: ";
-    cin >> target;
-    int e = arr.size() - 1;
-    if(binarySearch(arr, target,arr[0],arr[e]))
-    cout << "Element found in the array" << endl;
-    else
-    cout << "Element not found in the array" << endl;
+//     vector<int> arr = { 2,4,5,9,3,2,7,8};
+//     cout << "Binary Search: ";
+//     int target;
+//     cout << "Enter the target element to search: ";
+//     cin >> target;
+//     int e = arr.size() - 1;
+//     if(binarySearch(arr, target,arr[0],arr[e]))
+//     cout << "Element found in the array" << endl;
+//     else
+//     cout << "Element not found in the array" << endl;
     
-    cout << "Is array sorted? " << (isSorted(arr, n)? "Yes" : "No") << endl;
+//     cout << "Is array sorted? " << (isSorted(arr, n)? "Yes" : "No") << endl;
     
-    return 0;
+//     return 0;
     
-}
+// }
--- 44.cpp
+++ 44.cpp
@@ -1,7 +1,101 @@
 // RECURSION :- BACKTRACKING 
 
-#include<iostream>
-#include<vector>
-using namespace std;
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
 
-//print all subsets
+//print all subsets 1
+
+// void printSubsets(vector<int>& nums,vector<int>& result,int i){
+//     int n = nums.size();
+//     if(i == n){
+//         for(int num : result){
+//             cout<<num<<" ";
+//         }
+//         cout<<endl;
+//         return;
+//     }
+//     //include
+//     result.push_back(nums[i]);
+//     printSubsets(nums,result,i+1);
+
+//     result.pop_back(); //backtrack
+//     //exclude
+//     printSubsets(nums,result,i+1);
+
+// }//TC = O(2^n * n)
+
+// store subsets 1
+// void storeSubsets(vector<int>& nums,vector<vector<int>>& subsets,int i,vector<int>& result){
+//     int n = nums.size();
+//     if(i == n){
+//         subsets.push_back(result);
+//         return;
+//     }
+//     //include
+//     result.push_back(nums[i]);
+//     storeSubsets(nums,subsets,i+1,result);
+
+//     result.pop_back(); //backtrack
+//     //exclude
+//     storeSubsets(nums,subsets,i+1,result);
+
+// }//TC = O(2^n * n) 
+
+
+// subsets 2 (dupicate elements are present)
+// void getallsubsets(vector<int>&nums,vector<int>&ans,int i,vector<vector<int> >&allsubsets){
+//     int n = nums.size();
+//     if(i == n){
+//         allsubsets.push_back(ans);
+//         return;
+//     }
+//     //include
+//     ans.push_back(nums[i]);
+//     getallsubsets(nums,ans,i+1,allsubsets);
+//     ans.pop_back(); //backtrack
+
+//     int idx = i+1;
+//     while(idx < n && nums[idx] == nums[idx-1]) idx++;
+//     //exclude
+//     getallsubsets(nums,ans,idx,allsubsets);
+
+// }
+
+// vector<vector<int>> subsetswithdup(vector<int>& nums){
+//     sort(nums.begin(),nums.end());
+//     vector<vector<int>> allsubsets;
+//     vector<int> ans;
+//     getallsubsets(nums,ans,0,allsubsets);
+//     return allsubsets;
+// } // TC = O(2^n * n).
+
+
+int main()
+{
+    // vector<int> nums = {1,2,3};
+    // vector<int> result;
+    // printSubsets(nums,result,0);
+    // cout<<"\n";
+    // vector<vector<int>> subsets; 
+    // storeSubsets(nums,subsets,0,result);
+    // for(auto subset : subsets){
+    //     for(int num : subset){
+    //         cout<<num<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // vector<int> nums = {1,2,2};
+    // vector<vector<int>> subsets = subsetswithdup(nums);
+    // for(auto subset : subsets){
+    //     for(int num : subset){
+    //         cout<<num<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // return 0;
+
+ 
+}
