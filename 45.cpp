b4e91d2e47cbfb69d9fc553f316edf84177ba575
tejas mellimpudi <mellimpudi.tejas@gmail.com>
2025-02-02 10:20:06 +0530

    add

--- 45.cpp
+++ 45.cpp
@@ -1,1 +1,43 @@
-// permutations of array/string
+// // permutations of array/string
+
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+
+// // function to print permutations
+
+// void getPerms(vector<int>&nums, int idx, vector<vector<int>>&ans){
+//     int n = nums.size();
+//     if(idx == n){
+//         ans.push_back(nums);
+//         return;
+//     }
+//     for(int i=idx; i<n; i++){
+//         swap(nums[idx], nums[i]);
+//         getPerms(nums, idx+1, ans);
+
+//         swap(nums[idx], nums[i]); // backtrack
+//     }
+
+// }
+
+// vector<vector<int>> permute(vector<int>& nums){
+//     vector<vector<int>> ans;
+//     sort(nums.begin(), nums.end());
+//     getPerms(nums, 0, ans);
+//     return ans;
+//     // TC = O(n! * n)
+//     // SC = O(n!)
+// }
+
+// int main(){
+//     vector<int> nums = {1, 2, 3};
+//     vector<vector<int>> ans = permute(nums);
+//     for(auto& perm : ans){
+//         for(int num : perm)
+//             cout << num << " ";
+//         cout << endl;
+//     }
+//     return 0;
+// }
